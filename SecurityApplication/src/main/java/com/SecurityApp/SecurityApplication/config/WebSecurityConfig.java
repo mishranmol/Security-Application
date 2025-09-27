10032d84b6e702b613187034085d3f24e25c5827
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-09-27 18:19:32 +0530

    Update WebSecurityConfig.java

--- SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
+++ SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/config/WebSecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import static com.SecurityApp.SecurityApplication.enums.Role.ADMIN;
 
 @Configuration
-//By adding this we are telling the SpringBoot that we want to configure the SecurityFilterChain that SpringBoot has.
 @EnableWebSecurity
 public class WebSecurityConfig {
 
@@ -28,26 +27,6 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-// Note that when we define our WebSecurity and if we don't define our formLogin then our form will not work that is why its compulsory to define the form login
-//formLogin(Customizer.withDefaults()); // by doing this it will open form with default configuration means the form will work in its default manner like it was working before.
-
-// .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) -> after adding this line if we try to access any route then we will be redirected
-// to the login page and the page will be asking for login credentials but if we don't add this line then we can access any route directly without entering
-// the credentials inside the login page .
-
-// .requestMatchers("/posts").permitAll() ---> It means that all the posts requests will be public and anyone can access the posts requests without authenticating themselves.
-
-// .requestMatchers("/posts/**").permitAll() --->  These are the public routes , it means that we can access all the routes that comes under posts "without" entering
-// our credentials inside the LoginForm .
-
-// .requestMatchers("/posts/**").hasAnyRole("ADMIN") ---> It means that any route that comes under the posts will be accessed only by the user who has Role = "ADMIN" , mean to
-// say that anyone can access "/posts" route without authenticating themselves since we have done -> requestMatchers("/posts).permitAll() BUT if we will try to access
-// "/posts/1" then only the user having Role = "ADMIN" can access it .
-
-//.csrf( httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable() )  --> by writing so we are removing/disabling the Csrf token that is present inside our form .
-
-
-
         httpSecurity
                 .authorizeHttpRequests
                         (auth -> auth
@@ -58,9 +37,6 @@ public class WebSecurityConfig {
                                 .addFilterBefore(jwtAuthFilter , UsernamePasswordAuthenticationFilter.class);
 //                                .formLogin(Customizer.withDefaults());
 
-
-
-        //we are returning the SecurityFilterChain
         return httpSecurity.build();
     }
 
