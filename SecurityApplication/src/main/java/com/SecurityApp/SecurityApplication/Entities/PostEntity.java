a8349a39843a2aa556a847f5f9cab3b31665280d
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-09-27 16:23:23 +0530

    Update PostEntity.java

--- SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/Entities/PostEntity.java
+++ SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/Entities/PostEntity.java
@@ -6,11 +6,8 @@ import lombok.*;
 
 @Entity
 @Table(name = "posts")
-
-// these 2 constructors are also needed by hibernate to convert entity into table
 @NoArgsConstructor
 @AllArgsConstructor
-//to convert the DTO back to entity we have to use getter and setter
 @Getter
 @Setter
 public class PostEntity {
