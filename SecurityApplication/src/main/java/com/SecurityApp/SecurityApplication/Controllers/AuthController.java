db8e2a9ecb69369b12cb9bdd81cd96366559bcf6
Anmol Mishra <97759302+mishranmol@users.noreply.github.com>
2025-09-27 16:20:34 +0530

    Update AuthController.java

--- SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/Controllers/AuthController.java
+++ SecurityApplication/src/main/java/com/SecurityApp/SecurityApplication/Controllers/AuthController.java
@@ -39,59 +39,24 @@ public class AuthController {
     }
 
     @PostMapping(path = "/login")
-    //we were having a circular dependency when we have written this login method code inside the UserService hence we are writing inside authService .
-    //we are passing the HttpServletResponse object to save our token inside a cookie
     // login method inside AuthService
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto , HttpServletResponse response){
 
-        // before lecture 6.1(refresh token and access token) we were getting only JWT(i.e->accessToken) in response but now in lecture 6.1
-        // we are getting 2 tokens(accessToken and refreshToken) in response .
-        // String token = authService.login(loginDto);
-
-
-        //This loginResponseDto contains both the tokens(access and refresh)
         LoginResponseDto loginResponseDto = authService.login(loginDto);
-
-        // before lecture 6.1 , creating a cookie to save the JWT(accessToken)
-        // Cookie cookie = new Cookie("token" , token);
-
-        //but now in lecture 6.1 , we have to save the refreshToken inside the cookie
+        
         Cookie cookie = new Cookie("refreshToken" , loginResponseDto.getRefreshToken());
 
-        // we can set a lot of information about this cookie .
-        // we are doing setHttpOnly(true); so that this cookie cannot be accessed by any another means , and it can only be found with help of
-        // HTTP methods and only the loggedIn user can have access to this cookie .
         // httpOnly cookies can be passed from the backend to the frontend .
         cookie.setHttpOnly(true);
 
-
-        //adding the cookie as a part of response as this cookie will be containing our token .
         response.addCookie(cookie);
 
-        //now we can see inside our postman that when we will be hitting the "/login" route then we will be getting a cookie also in response .
         return ResponseEntity.ok(loginResponseDto);
     }
 
 
     @PostMapping(path = "/refresh")
     public ResponseEntity<LoginResponseDto> refresh(HttpServletRequest request){
-
-        //request.getCookies() return's array of cookies
-        // so we will iterate over all the cookies and get the cookie whose name is "refreshToken"
-        // Arrays.stream takes in array as input and convert it to stream
-        // What .filter(...) does in a Stream ???
-        //Stream<T> filter(Predicate<? super T> predicate)
-        //Accepts a Predicate<T> (a lambda returning true or false for each item).
-        //Returns a new stream containing only elements for which the predicate is true
-
-        //example -> List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
-        //nums.stream()
-        //    .filter(n -> n % 2 == 0)  // keeps only even numbers
-        //    .forEach(System.out::println);
-
-        // cookie constructor contains 2 things -> name and value
-        // findFirst() gives the first result
-        // map is used to map the cookie to the value
 
       String refreshToken =  Arrays.stream(request.getCookies())
                 .filter(cookie -> "refreshToken".equals(cookie.getName()))
@@ -104,16 +69,6 @@ public class AuthController {
 
     }
 
-//    @PostMapping(path = "/logout")
-//    public void logout(@RequestBody String refreshToken){
-//        authService.logout(refreshToken);
-//    }
-//
-//    public void logout(String refreshToken) {
-//
-//        Session session = sessionService.getSessionByRefreshToken(refreshToken);
-//        sessionService.
-//    }
 
     @DeleteMapping(path = "delete/{Id}")
     public void deleteUserById(@PathVariable Long Id) throws ResourceNotFoundException {
